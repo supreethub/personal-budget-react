b660ffb604cac40d1896cc9680c30471cf7a7950
Supreeth <supreethub96@gmail.com>
2020-10-14 19:35:41 -0400

    Brought PBudget in

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function AboutPage() {
+  return (
+    <div>
+        About Page!
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/Menu/Menu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+    Link
+  } from "react-router-dom";
+
+
+function Menu() {
+  return (
+    <nav className="fn-nav"
+        role = "navigation"
+        aria-label = "Main menu"
+        itemScope
+        itemType = "https://schema.org/SiteNavigationElement"
+    >   
+         <ul>
+                
+                <li><Link itemProp="url" to="/">Home</Link></li>
+                <li><Link itemProp="url" to="/about">About</Link></li>
+                <li><Link itemProp="url" to="/login">Login</Link></li>
+            </ul>
+        </nav>
+  );
+}
+
+export default Menu;
